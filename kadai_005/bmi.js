9d94cb834e45781d80de2b2bd86010fd4a68d94e
myzw-ai <myzw.ai110@gmail.com>
2024-07-30 22:13:31 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,23 @@
+//体重の宣言
+let bodyWeight;
+
+bodyWeight = 68;
+
+//身長の宣言
+let height;
+
+height = 1.7;
+
+//BMIの宣言
+
+let bmi;
+
+bmi = bodyWeight / (height * height);
+
+//コンソールへの出力
+console.log(bmi);
+
+
+
+
+
